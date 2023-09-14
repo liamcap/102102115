27fd7d4ae63e247e65b937bcf142618b0069d785
liamcap <143771403+liamcap@users.noreply.github.com>
2023-09-14 16:31:58 +0800

    加入main函数

--- bilibili.py
+++ bilibili.py
@@ -10,15 +10,8 @@ from wordcloud import WordCloud
 import numpy
 from PIL import Image
 
-content_list = []  # 存储获取到的弹幕
-top_twenty_words = []  # 存储词频最高的二十个词
-
-with open('stopwords.txt', 'r', encoding='utf-8') as file:
-    stop_words = set([line.strip() for line in file])
-
-
-def count_and_write_to_excel(content_list, excel_file):
-    global top_twenty_words
+def count_and_write_to_excel(content_list, excel_file, stop_words):  # 统计词频并写入Excel文件
+    top_twenty_words = []
     # 分词并去除空格、特殊字符和停用词
     words = [word.strip() for content in content_list for word in jieba.cut(content) if
              word.strip() and word not in stop_words and word.strip() not in string.punctuation]
@@ -27,10 +20,9 @@ def count_and_write_to_excel(content_list, excel_file):
     df = pd.DataFrame(top_twenty_words, columns=["词汇", "频率"])  # 直接创建 DataFrame
     df = df.sort_values(by="频率", ascending=False)  # 根据频率降序排序
     df.to_excel(excel_file, index=False, engine='openpyxl')  # 写入Excel文件
+    return top_twenty_words
 
-
-def getdm(url):  # 从指定url获取弹幕存储在content_list
-    global content_list
+def getdm(url, content_list):  # 从指定url获取弹幕存储在content_list
     headers = {  # 请求表头
         'user-agent': ''
     }
@@ -39,7 +31,6 @@ def getdm(url):  # 从指定url获取弹幕存储在content_list
     content = re.findall('<d p=".*?">(.*?)</d>', response.text)  # 正则表达式找出想要的内容
     for dms in content:
         content_list.append(dms)
-
 
 def bvid2cid(bvid):  # 将BV号转化为cid
     url = f"https://api.bilibili.com/x/player/pagelist?bvid={bvid}&jsonp=jsonp"
@@ -59,7 +50,6 @@ def bvid2cid(bvid):  # 将BV号转化为cid
     except (KeyError, IndexError) as e:
         print(f"提取cid时出现错误：{e}")
         return None
-
 
 def get_bvs_and_cids(keyword, desired_count):
     headers = {
@@ -97,25 +87,34 @@ def get_bvs_and_cids(keyword, desired_count):
 
     return cids
 
-keyword = '日本核污染水排海'  # 爬取视频弹幕的主题
-desired_count = 20  # 总共需要获取的BV号数量
-cids = get_bvs_and_cids(keyword, desired_count)
+def main():
+    content_list = []  # 存储获取到的弹幕
 
-for i in range(desired_count):
-    oid = cids[i]
-    url = 'https://api.bilibili.com/x/v1/dm/list.so?oid=' + str(oid)  # 弹幕网址
-    getdm(url)
-    print(i)  # 显示获取进度
+    with open('stopwords.txt', 'r', encoding='utf-8') as file:
+        stop_words = set([line.strip() for line in file])
 
-#  存储到excel文件
-excel_file = "danmu_word_counts.xlsx"
-count_and_write_to_excel(content_list, excel_file)
+    keyword = '日本核污染水排海'  # 爬取视频弹幕的主题
+    desired_count = 20  # 总共需要获取的BV号数量
+    cids = get_bvs_and_cids(keyword, desired_count)
 
-# 显示词云图
-color_mask = numpy.array(Image.open('whale.jpg'))  # 想要做成的形状
-wordcloud = WordCloud(mask=color_mask, font_path='simsun.ttc', background_color='white').generate_from_frequencies(
-    dict(top_twenty_words))
-plt.figure(figsize=(8, 8))
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis('off')
-plt.show()
+    for i in range(desired_count):
+        oid = cids[i]
+        url = 'https://api.bilibili.com/x/v1/dm/list.so?oid=' + str(oid)  # 弹幕网址
+        getdm(url, content_list)
+        print(i)  # 显示获取进度
+
+    # 存储到excel文件
+    excel_file = "danmu_word_counts.xlsx"
+    top_twenty_words = count_and_write_to_excel(content_list, excel_file, stop_words)
+
+    # 显示词云图
+    color_mask = numpy.array(Image.open('whale.jpg'))  # 想要做成的形状
+    wordcloud = WordCloud(mask=color_mask, font_path='simsun.ttc', background_color='white').generate_from_frequencies(
+        dict(top_twenty_words))
+    plt.figure(figsize=(8, 8))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    plt.show()
+
+if __name__ == "__main__":
+    main()
