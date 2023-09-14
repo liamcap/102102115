f446d4eb5199e61c24fa7ad443691664c4b0f6a7
liamcap <143771403+liamcap@users.noreply.github.com>
2023-09-14 15:44:50 +0800

    Update bilibili.py

--- bilibili.py
+++ bilibili.py
@@ -32,7 +32,7 @@ def count_and_write_to_excel(content_list, excel_file):
 def getdm(url):  # 从指定url获取弹幕存储在content_list
     global content_list
     headers = {  # 请求表头
-        'user-agent': '' # 浏览器的user-agent信息
+        'user-agent': ''
     }
     response = requests.get(url=url, headers=headers)  # 发送请求
     response.encoding = 'utf-8'  # 解决乱码
@@ -61,41 +61,45 @@ def bvid2cid(bvid):  # 将BV号转化为cid
         return None
 
 
-headers = {
-    'User-Agent': '',  # 浏览器的user-agent信息
-    'Cookie': ""}  # 浏览器的cookie信息
+def get_bvs_and_cids(keyword, desired_count):
+    headers = {
+        'User-Agent': '',
+        'Cookie': ''
+    }
+
+    current_count = 0  # 当前已获取的BV号数量
+    bvs = []  # 存储所有获取的BV号
+    cids = []  # 存储所有获取的BV号转换为cid
+    page = 1  # 初始页码
+
+    while current_count < desired_count:
+        search_url = f'https://api.bilibili.com/x/web-interface/search/all?keyword={keyword}&page={page}&order='
+        response = requests.get(search_url, headers=headers)
+        data = response.text
+        BVs = re.findall('BV..........', data)
+        bvs.extend(BVs)
+        current_count += len(BVs)
+        page += 1
+        if len(BVs) == 0:
+            break
+        print(search_url)
+        time.sleep(1)
+
+    print(f'获取到了 {len(bvs)} 个BV号：')
+
+    for bvid in bvs:
+        cid = bvid2cid(bvid)
+        time.sleep(0.1)
+        cids.append(cid)
+        print(len(cids))
+        if len(cids) == desired_count:
+            break
+
+    return cids
 
 keyword = '日本核污染水排海'  # 爬取视频弹幕的主题
-desired_count = 300  # 总共需要获取的BV号数量
-current_count = 0  # 当前已获取的BV号数量
-bvs = []  # 存储所有获取的BV号
-cids = []  # 存储所有获取的BV号转换为cid
-page = 1  # 初始页码
-
-while current_count < desired_count:
-    search_url = f'https://api.bilibili.com/x/web-interface/search/all?keyword={keyword}&page={page}&order='  # 发送搜索请求获取视频信息
-    response = requests.get(search_url, headers=headers)
-    data = response.text
-    BVs = re.findall('BV..........', data)  # 正则表达式提取出本页面所有的BV号
-    bvs.extend(BVs)
-    current_count += len(BVs)
-    page += 1  # 继续爬取下一页
-    # 如果本页没有视频获取不到bv号就停止
-    if len(BVs) == 0:
-        break
-    print(search_url)
-    time.sleep(1)  # 防止访问太频繁
-
-print(f'获取到了 {len(bvs)} 个BV号：')
-
-for bvid in bvs:
-    # 获取视频的cid，并将其附加到cids列表中
-    cid = bvid2cid(bvid)
-    time.sleep(0.1)
-    cids.append(cid)
-    print(len(cids))  # 通过数字显示进度
-    if len(cids) == desired_count:  # 获取到足够的cid就停止
-        break
+desired_count = 20  # 总共需要获取的BV号数量
+cids = get_bvs_and_cids(keyword, desired_count)
 
 for i in range(desired_count):
     oid = cids[i]
@@ -106,9 +110,9 @@ for i in range(desired_count):
 #  存储到excel文件
 excel_file = "danmu_word_counts.xlsx"
 count_and_write_to_excel(content_list, excel_file)
-color_mask = numpy.array(Image.open('whale.jpg'))  # 想要做成的形状
 
 # 显示词云图
+color_mask = numpy.array(Image.open('whale.jpg'))  # 想要做成的形状
 wordcloud = WordCloud(mask=color_mask, font_path='simsun.ttc', background_color='white').generate_from_frequencies(
     dict(top_twenty_words))
 plt.figure(figsize=(8, 8))
