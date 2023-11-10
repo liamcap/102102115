adba7eebdadf5f23cbc602583ac2e02534430e00
liamcap <143771403+liamcap@users.noreply.github.com>
2023-11-10 20:56:13 +0800

    Update models.py

--- mysite/app/models.py
+++ mysite/app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class UserInfo(models.Model):
+    name = models.CharField(max_length=32)
+    password = models.CharField(max_length=64)
+    age =models.IntegerField(default=2)
+    likes = models.IntegerField(null=True, blank=True)
+
+'''
+create table app_userinfo(
+    id bigint auto_increment primary key,
+    name varchar(32),
+    password varchar(64),
+    age int dedault 2
+)
+'''
+
+'''
+执行命令
+python manage.py makemigrations
+python manage.py migrate
+'''
