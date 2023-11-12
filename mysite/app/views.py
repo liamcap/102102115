8f7cd19f5b61c89c37830d207e1e48ae8f84c99b
liamcap <143771403+liamcap@users.noreply.github.com>
2023-11-12 20:08:24 +0800

    Update views.py

--- mysite/app/views.py
+++ mysite/app/views.py
@@ -1,14 +1,51 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
+from django.shortcuts import render, HttpResponse, redirect
+from app.models import UserInfo
 
 def index(request):
     return HttpResponse("欢迎使用")
 
-def user_list(request):
+# def user_list(request):
 
-    # 默认app目录下寻找templates
-    return render(request, "user_list.html")
+#     # 默认app目录下寻找templates
+#     return render(request, "user_list.html")
 
-def user_add(request):
-    return HttpResponse("添加用户")
+# def user_add(request):
+#     return HttpResponse("添加用户")
+
+# def tpl(request):
+#     name = "taylor"
+#     roles = ["the", "eras", "tour"]
+#     user_info = {"name":"swift", "salary":1000}
+#     return render(request, "tpl.html", {"n1":name, "n2":roles, "n3":user_info})
+
+def orm(request):
+    UserInfo.objects.all().delete()
+    UserInfo.objects.create(name="Liam", password="999")
+    UserInfo.objects.create(name="Cap", password="1221", likes=5)
+    return HttpResponse("success")
+
+def info_list(request):
+
+    data_list = UserInfo.objects.all()
+    # 渲染返回给用户
+    return render(request, "User_info.html", {"data_list":data_list})
+
+def info_add(request):
+    if request.method == "GET":
+        return render(request, 'User_add.html')
+    
+    # 获取信息
+    user_name = request.POST.get("user")
+    pwd = request.POST.get("password")
+    age = request.POST.get("age")
+    likes = request.POST.get("likes")
+
+    # 添加到数据库
+    UserInfo.objects.create(name=user_name,password=pwd,age=age,likes=likes)
+
+    return redirect("/info/list/")
+
+def info_delete(request):
+    nid = request.GET.get("nid")
+    UserInfo.objects.filter(id=nid).delete()
+    return redirect("/info/list/")
