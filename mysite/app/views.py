23ffd62243171e7ff428559349fae76cc641e9c5
liamcap <143771403+liamcap@users.noreply.github.com>
2023-11-21 21:17:34 +0800

    Update views.py

--- mysite/app/views.py
+++ mysite/app/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render, HttpResponse, redirect
 from app.models import UserInfo
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.request import Request
+import uuid
 
 def index(request):
     return HttpResponse("欢迎使用")
+
+# def user_list(request):
+
+#     # 默认app目录下寻找templates
+#     return render(request, "user_list.html")
+
+# def user_add(request):
+#     return HttpResponse("添加用户")
+
+# def tpl(request):
+#     name = "taylor"
+#     roles = ["the", "eras", "tour"]
+#     user_info = {"name":"swift", "salary":1000}
+#     return render(request, "tpl.html", {"n1":name, "n2":roles, "n3":user_info})
 
 def orm(request):
     UserInfo.objects.all().delete()
@@ -35,3 +53,19 @@ def info_delete(request):
     nid = request.GET.get("nid")
     UserInfo.objects.filter(id=nid).delete()
     return redirect("/info/list/")
+
+class LoginView(APIView):
+    authrntication_classs = []
+
+    def post(self, request):
+        user = request.data.get("username")
+        pwd = request.data.get("password")
+        user_object = UserInfo.objects.filter(name=user, password=pwd).first()
+        if not user_object:
+            return Response({"code":1001, 'msg':"密码错误"})
+        
+        token = str(uuid.uuid4())
+        user_object.token = token
+        user_object.save()
+        
+        return Response({"status": True, 'data': token})
