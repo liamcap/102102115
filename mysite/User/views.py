58d5e416aab4e22f1d88946e92fdd901302eae89
liamcap <143771403+liamcap@users.noreply.github.com>
2023-11-21 21:16:14 +0800

    Update views.py

--- mysite/User/views.py
+++ mysite/User/views.py
@@ -1,203 +1,71 @@
-from rest_framework import status
+from django.shortcuts import render, HttpResponse, redirect
+from app.models import UserInfo
 from rest_framework.views import APIView
-
 from rest_framework.response import Response
-from User.models import Users, History, Favorites
-import os
-from Ingredient.models import Ingredient
-import requests
-from FoodDetect.settings import APPID, SECRET, MEDIA_ROOT
-import json
+from rest_framework.request import Request
 import uuid
-from Ingredient.serializers import IngredientSerializer, IngredientOcrSerializer
-from User.serializers import ReviewFavorViewSerializer
 
+def index(request):
+    return HttpResponse("欢迎使用")
 
-# Create your views here.
+# def user_list(request):
 
-class OCRIngredient(APIView):
-    '''
-    这个view类是用户上传图片，然后通过ocr算法，返回图片内容
-    '''
+#     # 默认app目录下寻找templates
+#     return render(request, "user_list.html")
 
-    def post(self, request):
-        '''
-        username
-        file1
-        :param request:
-        :return:
-        '''
-        # 获取用户信息
-        username = request.data.get('username')
-        user = Users.objects.filter(name=username).first()
-        if user is None:
-            return Response({'error': '您未注册，请先注册'},
-                            status=status.HTTP_400_BAD_REQUEST)
-        # 获取图片
-        pic = request.data.get('file1')
-        type = pic.content_type
-        if type not in ['image/jpg', 'image/png', 'image/jpeg']:
-            return Response({'error': '您上传的照片不支持，只支持jpg、png、jpeg格式的照片'},
-                            status=status.HTTP_400_BAD_REQUEST)
+# def user_add(request):
+#     return HttpResponse("添加用户")
 
-        file_extension = pic.name.split('.')[-1]
-        unique_filename = str(uuid.uuid4()) + '.' + file_extension
-        # 构建目标文件夹路径
-        target_folder = MEDIA_ROOT
-        # 确保目标文件夹存在
-        os.makedirs(target_folder, exist_ok=True)
+# def tpl(request):
+#     name = "taylor"
+#     roles = ["the", "eras", "tour"]
+#     user_info = {"name":"swift", "salary":1000}
+#     return render(request, "tpl.html", {"n1":name, "n2":roles, "n3":user_info})
 
-        # 构建目标文件路径
-        target_path = os.path.join(target_folder, unique_filename)
+def orm(request):
+    UserInfo.objects.all().delete()
+    UserInfo.objects.create(name="Liam", password="999")
+    UserInfo.objects.create(name="Cap", password="1221", likes=5)
+    return HttpResponse("success")
 
-        # 保存文件
-        with open(target_path, 'wb') as f:
-            f.write(pic.read())
-        '''
-        经过校验后存储图片，并将图片发送给ocr算法
-        '''
-        # 假设ocr输出结果是[磷酸氢二钾,大豆分离蛋白]
-        results = ['磷酸氢二钾', '大豆分离蛋白', 'wbg牛逼']
-        final_list = []
-        for item in results:
-            ingredient = Ingredient.objects.filter(chinese_name=item).first()
-            if ingredient:
-                serializer = IngredientOcrSerializer(instance=ingredient)
-                data = serializer.data
-                imageUrl = 'http://127.0.0.1:8000' + target_path
-                data['imgUrl'] = imageUrl
-                final_list.append(data)
-            else:
-                final_list.append({
-                    '配料名': item,
-                    'error': '抱歉，数据库中没有找到这个配料信息'})
+def info_list(request):
 
-        '''
-        存储历史记录，同时数据库事件回滚等
-        '''
-        his = History(user_id=user.user_id, image=imageUrl)
-        his.save()
-        return Response(final_list)
+    data_list = UserInfo.objects.all()
+    # 渲染返回给用户
+    return render(request, "User_info.html", {"data_list":data_list})
 
+def info_add(request):
+    if request.method == "GET":
+        return render(request, 'User_add.html')
+    
+    # 获取信息
+    user_name = request.POST.get("user")
+    pwd = request.POST.get("password")
+    age = request.POST.get("age")
+    likes = request.POST.get("likes")
+
+    # 添加到数据库
+    UserInfo.objects.create(name=user_name,password=pwd,age=age,likes=likes)
+
+    return redirect("/info/list/")
+
+def info_delete(request):
+    nid = request.GET.get("nid")
+    UserInfo.objects.filter(id=nid).delete()
+    return redirect("/info/list/")
 
 class LoginView(APIView):
-    '''
-    这个view类是进行用户的注册登录
-    '''
+    authrntication_classs = []
 
     def post(self, request):
-        '''
-        这个视图类需要用户姓名，以及前端传过来的js_code
-        username
-        js_code
-        :param request:
-        :return:
-        '''
-        username = request.data.get('username')
-        used = Users.objects.filter(name=username).first()
-        # 校验姓名
-        if used:
-            return Response({'error': '名字已存在，请更改你的名字'}, status=status.HTTP_400_BAD_REQUEST)
-        js_code = request.data.get('js_code')
-        appid = APPID
-        secret = SECRET
-        url = f"https://api.weixin.qq.com/sns/jscode2session?appid={appid}&secret={secret}&js_code={js_code}&grant_type=authorization_code"
-        res = requests.get(url).text
-        # 与微信API连接，获取openid与sessionKey
-        response_data = json.loads(res)
-        try:
-            session_key = response_data["session_key"]
-            openid = response_data["openid"]
-            user = Users(name=username, openid=openid)
-            user.save()
-            return Response({
-                'username': username,
-                'session_key': session_key
-            })
-        except Exception as err:
-            return Response(response_data)
-
-
-class RegisFavorView(APIView):
-    '''
-    这个view类是进行注册收藏夹
-    '''
-
-    def post(self, request):
-        '''
-        这个视图类需要用户名称，历史记录id，note记录
-        username
-        history_id
-        note
-        :param request:
-        :return:
-        '''
-        username = request.data.get('username')
-        history_id = request.data.get('history_id')
-        note = request.data.get('note')
-        # 获取用户信息
-        user = Users.objects.filter(name=username).first()
-        if user:
-            his = History.objects.filter(history_id=history_id).filter(user_id=user.user_id).first()
-            if his:
-                fav = Favorites(user_id=user.user_id, history_id_id=his.history_id, note=note)
-                fav.save()
-                return Response(data=request.data)
-            else:
-                return Response({'error': '历史记录不存在，请重新拍照片上传生成记录'},
-                                status=status.HTTP_400_BAD_REQUEST)
-        else:
-            return Response({'error': '您未注册，请先注册'},
-                            status=status.HTTP_400_BAD_REQUEST)
-
-
-class ReviewFavorView(APIView):
-    '''
-    这个视图类进行返回用户收藏夹记录:还有bug
-    '''
-
-    def get(self, request):
-        '''
-        username
-        favorites_id
-        :param request:
-        :return:
-        '''
-        # 获取用户姓名
-        username = request.query_params.get('username')
-        # 获取收藏夹id
-        favorites_id = request.query_params.get('favorites_id')
-        user = Users.objects.filter(name=username).first()
-        if user:
-            # 获取数据
-            fav = Favorites.objects.filter(favorites_id=favorites_id).filter(user_id=user.user_id).first()
-            if fav:
-                # 获取history_id查找历史记录
-                history_id = fav.history_id.history_id
-                # 获取土拍你url地址
-                history = History.objects.filter(history_id=history_id).first()
-                img_url = history.image.path
-                '''
-                将图片重新传给OCR算法
-                '''
-                # 假设算法输出的结果为下
-                results = ['磷酸氢二钾', '大豆分离蛋白', 'wbg牛逼']
-                final_list = []
-                for item in results:
-                    ingredient = Ingredient.objects.filter(chinese_name=item).first()
-                    if ingredient:
-                        serializer = IngredientOcrSerializer(instance=ingredient)
-                        data = serializer.data
-                        data['imgUrl'] = img_url
-                        final_list.append(data)
-                    else:
-                        final_list.append({
-                            '配料名': item,
-                            'error': '抱歉，数据库中没有找到这个配料信息'})
-                return Response(final_list)
-            else:
-                return Response({'error': '没有这项收藏记录，请先进行收藏'},
-                                status=status.HTTP_400_BAD_REQUEST)
-        else:
-            return Response({'error': '您未注册，请先注册'},
-                            status=status.HTTP_400_BAD_REQUEST)
+        user = request.data.get("username")
+        pwd = request.data.get("password")
+        user_object = UserInfo.objects.filter(name=user, password=pwd).first()
+        if not user_object:
+            return Response({"code":1001, 'msg':"密码错误"})
+        
+        token = str(uuid.uuid4())
+        user_object.token = token
+        user_object.save()
+        
+        return Response({"status": True, 'data': token})
