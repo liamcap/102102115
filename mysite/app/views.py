47dda7b83fe7e026aaa05042f2b816eedd34ba63
liamcap <143771403+liamcap@users.noreply.github.com>
2023-11-28 21:19:05 +0800

    Update views.py

--- mysite/app/views.py
+++ mysite/app/views.py
@@ -4,15 +4,30 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.request import Request
 import uuid
+from rest_framework import serializers
 
 def index(request):
     return HttpResponse("欢迎使用")
 
-def orm(request):
-    UserInfo.objects.all().delete()
-    UserInfo.objects.create(name="Liam", password="999")
-    UserInfo.objects.create(name="Cap", password="1221", likes=5)
-    return HttpResponse("success")
+# def user_list(request):
+
+#     # 默认app目录下寻找templates
+#     return render(request, "user_list.html")
+
+# def user_add(request):
+#     return HttpResponse("添加用户")
+
+# def tpl(request):
+#     name = "taylor"
+#     roles = ["the", "eras", "tour"]
+#     user_info = {"name":"swift", "salary":1000}
+#     return render(request, "tpl.html", {"n1":name, "n2":roles, "n3":user_info})
+
+# def orm(request):
+#     UserInfo.objects.all().delete()
+#     UserInfo.objects.create(name="Liam", password="999")
+#     UserInfo.objects.create(name="Cap", password="1221", likes=5)
+#     return HttpResponse("success")
 
 def info_list(request):
 
@@ -33,23 +48,46 @@ def info_add(request):
     # 添加到数据库
     UserInfo.objects.create(name=user_name,password=pwd,age=age,likes=likes)
 
-    return redirect("/info/list/")
+    return redirect("/user/list/")
 
 def info_delete(request):
+    # http://127.0.0.1:8000/user/delete/?nid=
     nid = request.GET.get("nid")
     UserInfo.objects.filter(id=nid).delete()
-    return redirect("/info/list/")
+    return redirect("/user/list/")
+
+class infoSerializer(serializers.Serializer):
+
+    # 名称必须与models里的相同
+    name = serializers.CharField()
+    password = serializers.CharField()
+    age = serializers.IntegerField()
+
+class infoView(APIView):
+
+    def get(self, request, *args, **kwargs):
+        # 获取数据
+        user_object = UserInfo.objects.all().first()
+        
+        # 序列化成json
+        ser = infoSerializer(instance=user_object)
+
+        # 返回给用户
+        context = {"status":True, "data":ser.data}
+        return Response(context)
 
 class LoginView(APIView):
     authrntication_classs = []
 
     def post(self, request):
+
         user = request.data.get("username")
         pwd = request.data.get("password")
+        # 校验登录是否成功
         user_object = UserInfo.objects.filter(name=user, password=pwd).first()
         if not user_object:
             return Response({"code":1001, 'msg':"密码错误"})
-        
+        # 登录成功 
         token = str(uuid.uuid4())
         user_object.token = token
         user_object.save()
