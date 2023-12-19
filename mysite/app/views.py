e83669740a768ec6e78032f51c111ba631ff6ed0
liamcap <143771403+liamcap@users.noreply.github.com>
2023-12-19 20:47:40 +0800

    Update views.py

--- mysite/app/views.py
+++ mysite/app/views.py
@@ -9,15 +9,6 @@ from rest_framework import serializers
 def index(request):
     return HttpResponse("欢迎使用")
 
-# def user_list(request):
-
-#     # 默认app目录下寻找templates
-#     return render(request, "user_list.html")
-
-# def user_add(request):
-#     return HttpResponse("添加用户")
-
-
 
 def info_list(request):
 
